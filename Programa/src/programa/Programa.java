4d0aca05b9fb8a47f175b236febc27e72d4e45f4
GustavoDomenech <gustavo.domenechs@gmail.com>
2025-10-27 19:38:13 -0300

    :O

--- Programa/src/programa/Programa.java
+++ Programa/src/programa/Programa.java
@@ -14,7 +14,7 @@ public class Programa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO todo codigo
     }
     
 }
