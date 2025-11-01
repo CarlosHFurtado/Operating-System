198241ffb3071ded8085c914f72c75c9ffa2eb21
Dyy <156555666+DLedebuhr@users.noreply.github.com>
2025-11-01 18:26:51 -0300

    Create ADDR.java

---
+++ Programa/src/Instrucoes/ADDR.java
@@ -0,0 +1,56 @@
+package Instrucoes;
+
+import Executor.Memoria;
+import Executor.Registradores;
+
+public class ADDR extends Instrucao {
+
+    public ADDR() {
+        
+        super("ADDR", (byte)0x90, "2", 2);
+    
+    }
+
+    @Override
+    public void executar(Memoria memoria, Registradores registradores) {
+        
+        int pcAtual = registradores.getValor("PC");
+        
+        byte[] bytesInstrucao = memoria.getBytes(pcAtual, 2);
+        
+        int[] registradoresID = getRegistradores(bytesInstrucao);
+        int r1 = registradoresID[0]; // Registrador fonte
+        int r2 = registradoresID[1]; // Registrador destino
+        
+        String nomeR1 = getNomeRegistrador(r1);
+        String nomeR2 = getNomeRegistrador(r2);
+        
+        int valorR1 = registradores.getValor(nomeR1);
+        int valorR2 = registradores.getValor(nomeR2);
+        
+        int resultado = valorR2 + valorR1;
+        
+        registradores.setValor(nomeR2, resultado);
+        
+        registradores.incrementar("PC", 2);
+        
+    }
+    
+    private String getNomeRegistrador(int id) {
+        
+        switch(id) {
+            
+            case 0: return "A";
+            case 1: return "X"; 
+            case 2: return "L";
+            case 3: return "B";
+            case 4: return "S";
+            case 5: return "T";
+            case 6: return "F";
+            case 8: return "PC";
+            case 9: return "SW";
+            default: return "A"; 
+            
+        }
+    }
+}
