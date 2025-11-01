96ebfa8ae8fc959b4e9542b8a4130194b7e9d275
Dyy <156555666+DLedebuhr@users.noreply.github.com>
2025-11-01 15:03:54 -0300

    ADD

--- Programa/src/Executor/Teste.java
+++ Programa/src/Executor/Teste.java
@@ -10,4 +10,20 @@ package Executor;
  */
 public class Teste {
     
+    
+    2 bytes - Operações entre registradores
+    3 bytes - Usa endereçamento relativo
+    4 bytes - estendido
+            
+            
+    Primeiro byte = opcode (6) | n (1) | i (1)
+            
+    Segundo byte = x (1) | b (1) | p (1) | e (1) | Resto do Endereço (4)
+
+    x = Soma do registrador X
+    p = Endereçamento relativo ao PC
+    b = Endereçamento relativo a BASE
+    e = Determina se é formato 4
+
+
 }
