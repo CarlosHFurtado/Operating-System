68c451526a1093be99398b87c51186333cac4997
Madu🐸 <dudapelrs123@gmail.com>
2025-11-28 14:33:22 -0300

    hum

--- Programa/src/Instrucoes/InstrucaoFormato2.java
+++ Programa/src/Instrucoes/InstrucaoFormato2.java
@@ -15,8 +15,6 @@ public abstract class InstrucaoFormato2 extends Instrucao {
     
     protected int[] obterRegistradores(Memoria memoria, Registradores registradores) {
         
-        // O PC já aponta para o byte do opcode, precisamos dos próximos 2 bytes 
-        // No caso, apenas 1 byte após o opcode, pois o formato 2 tem 2 bytes no total
         
         byte[] bytes = memoria.getBytes(2, registradores.getValor("PC"));        
           
